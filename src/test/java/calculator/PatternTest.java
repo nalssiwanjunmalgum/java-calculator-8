fa2095ae336c0f9c2dc883ece2ba8307a71ae8bc
sunghun <nokdonwithkj211@gmail.com>
2025-10-15 16:07:55 +0900

    Test(PatternTest): 기본 구분자 관련 추가 테스트 케이스 작성

--- src/test/java/calculator/PatternTest.java
+++ src/test/java/calculator/PatternTest.java
@@ -3,30 +3,51 @@ package calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class PatternTest {
-    @Test
-    @DisplayName("기본 구분자(콤마)와 양수로 구성되어 있으면 성공")
-    void when_include_basic_delimeter_COMMA() {
-        BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
-        boolean isValidatedPattern = basicPatternValidator.validate("1,2,3");
-        assertThat(isValidatedPattern).isEqualTo(true);
-    }
 
-    @Test
-    @DisplayName("기본 구분자(콜론)와 양수로 구성되어 있으면 성공")
-    void when_include_basic_delimeter_COLON() {
-        BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
-        boolean isValidatedPattern = basicPatternValidator.validate("1:2:3");
-        assertThat(isValidatedPattern).isEqualTo(true);
-    }
+    @Nested
+    class BasicPattern {
+        @Test
+        @DisplayName("기본 구분자(콤마)와 양수로 구성되어 있으면 성공")
+        void when_include_basic_delimeter_COMMA() {
+            BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
+            boolean isValidatedPattern = basicPatternValidator.validate("1,2,3");
+            assertThat(isValidatedPattern).isEqualTo(true);
+        }
 
-    @Test
-    @DisplayName("기본 구분자여도 양수 만족하지 못하면 실패")
-    void when_include_0_in_front_of_positive_number() {
-        BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
-        boolean isValidatedPattern = basicPatternValidator.validate("1, 02, 3");
-        assertThat(isValidatedPattern).isEqualTo(false);
+        @Test
+        @DisplayName("기본 구분자(콜론)와 양수로 구성되어 있으면 성공")
+        void when_include_basic_delimeter_COLON() {
+            BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
+            boolean isValidatedPattern = basicPatternValidator.validate("1:2:3");
+            assertThat(isValidatedPattern).isEqualTo(true);
+        }
+
+        @Test
+        @DisplayName("기본 구분자여도 양수 만족하지 못하면 실패")
+        void when_include_0_in_front_of_positive_number() {
+            BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
+            boolean isValidatedPattern = basicPatternValidator.validate("1,02,3");
+            assertThat(isValidatedPattern).isEqualTo(false);
+        }
+
+        @Test
+        @DisplayName("기본 구분자여도 음수가 존재하면 실패")
+        void when_include_negative_number() {
+            BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
+            boolean isValidatedPattern = basicPatternValidator.validate("-11,2,3");
+            assertThat(isValidatedPattern).isEqualTo(false);
+        }
+
+        @Test
+        @DisplayName("기본 구분자가 섞여있으면 실패")
+        void when_mixed_comma_colon() {
+            BasicPatternValidator basicPatternValidator = new BasicPatternValidator();
+            boolean isValidatedPattern = basicPatternValidator.validate("1,2:3");
+            assertThat(isValidatedPattern).isEqualTo(false);
+        }
     }
 }
