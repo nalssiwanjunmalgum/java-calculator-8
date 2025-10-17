f9d2c3165ef67a49192f54e100725c7a6061f20d
sunghun <nokdonwithkj211@gmail.com>
2025-10-17 16:53:06 +0900

    Refactor(): 이스케이프 반영한 정규시 표현 수정, 기본과 커스텀 패턴으로 구분

---
+++ src/main/java/calculator/CommonPatternValidator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import java.util.regex.Pattern;
+
+public class CommonPatternValidator {
+    private static final Pattern COMMON_PATTERN = Pattern.compile("^[1-9][0-9]*([,:][1-9][0-9]*)*$");
+
+    public boolean validate(String input) {
+        return COMMON_PATTERN.matcher(input).matches();
+    }
+}
--- src/main/java/calculator/CustomPatternValidator.java
+++ src/main/java/calculator/CustomPatternValidator.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CustomPatternValidator {
-    private static final Pattern CUSTOM_PATTERN = Pattern.compile("^(//)(.)(\\n)[1-9][0-9]*(\\2[1-9][0-9]*)+$");
+    private static final Pattern CUSTOM_PATTERN = Pattern.compile("^(//)(.)(\\\\n)[1-9][0-9]*(\\2[1-9][0-9]*)*$");
 
     public boolean validate(String s) {
         return CUSTOM_PATTERN.matcher(s).matches();
