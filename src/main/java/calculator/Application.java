8320c21d65500f4652deffc8dc78147bee8a759a
sunghun <nokdonwithkj211@gmail.com>
2025-10-17 17:02:23 +0900

    Feat(): 최종 메인 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        String userInput = inputView.getInput();
+        NumberExtractor numberExtractor = new NumberExtractor();
+        Calculator calculator = new Calculator();
+
+        List<Long> extractedNumbers = numberExtractor.extractFrom(userInput);
+        long result = calculator.execute(extractedNumbers);
+
+        outputView.printSum(result);
     }
 }
