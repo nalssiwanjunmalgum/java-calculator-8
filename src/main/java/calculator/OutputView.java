7d533194b0ab0e3c2048863659fdffc1bb965d3a
sunghun <nokdonwithkj211@gmail.com>
2025-10-17 16:50:03 +0900

    Feat(View): 출력과 입력을 담당하는 클래스 생성

---
+++ src/main/java/calculator/OutputView.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class OutputView {
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printSum(Long longValue) {
+        System.out.println("결과 : " + longValue);
+    }
+}
