50664fd2e02de7f1f5a809fc28769f22ff33f3fa
sunghun <nokdonwithkj211@gmail.com>
2025-10-17 16:57:42 +0900

    Feat(): Common, Basic 패턴으로 구분
    기본 구분자를 담당하는 COMMON_PATTERN
    기본 구분자를 공통으로 '*'로 치환한 BASIC_PATTERN

--- src/main/java/calculator/BasicPatternValidator.java
+++ src/main/java/calculator/BasicPatternValidator.java
@@ -3,7 +3,7 @@ package calculator;
 import java.util.regex.Pattern;
 
 public class BasicPatternValidator {
-    private static final Pattern BASIC_PATTERN = Pattern.compile("^[1-9][0-9]*(\\*[1-9][0-9]*)+$");
+    private static final Pattern BASIC_PATTERN = Pattern.compile("^[1-9][0-9]*(\\*[1-9][0-9]*)*$");
 
     public boolean validate(String input) {
         return BASIC_PATTERN.matcher(input).matches();
