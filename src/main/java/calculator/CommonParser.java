9397a0b68c36ff3db1e1fbf8380d1b69ad704a6f
sunghun <nokdonwithkj211@gmail.com>
2025-10-20 11:16:11 +0900

    Feat(): Parsing을 담당하는 클래스 생성. 공통, 커스텀으로 구분하여 생성

--- src/main/java/calculator/CommonParser.java
+++ src/main/java/calculator/CommonParser.java
@@ -1,4 +1,18 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class CommonParser {
+    private final String delimiter;
+
+    public CommonParser(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public List<Long> extractNumber(String input) {
+        String[] split = input.split(Pattern.quote(delimiter));
+        return Arrays.stream(split).mapToLong(Long::parseLong).boxed().toList();
+    }
 }
--- src/main/java/calculator/CustomParser.java
+++ src/main/java/calculator/CustomParser.java
@@ -1,4 +1,18 @@
 package calculator;
 
+import java.util.regex.Matcher;
+
 public class CustomParser {
+
+    public static Result parse(String input) {
+        Matcher m = RegexPatterns.CUSTOM.matcher(input);
+
+        if (m.matches()) {
+            String delimiter = m.group(1);   // 커스텀 구분자
+            String body = m.group(2); // 구분자 확인 제외한 문자열
+            return new Result(delimiter, body);
+        }
+
+        throw new IllegalArgumentException("파싱이 불가능합니다.");
+    }
 }
