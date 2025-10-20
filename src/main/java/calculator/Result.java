c111f2aa376d7df9dbd48cfc0a993b50b03eb1d8
sunghun <nokdonwithkj211@gmail.com>
2025-10-20 11:00:41 +0900

    Feat(Result): 파싱을 위한 최종 구분자, 문자열 정보를 가지고 있는 클래스 생성

--- src/main/java/calculator/Result.java
+++ src/main/java/calculator/Result.java
@@ -1,4 +1,20 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Result {
+    private final String delimiter;
+    private final String body;
+
+    public Result(String delimiter, String body) {
+        this.delimiter = delimiter;
+        this.body = body;
+    }
+
+    public List<Long> extractNumber() {
+        String[] split = body.split(Pattern.quote(delimiter));
+        return Arrays.stream(split).mapToLong(Long::parseLong).boxed().toList();
+    }
 }
